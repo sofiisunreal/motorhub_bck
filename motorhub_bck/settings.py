8e651cfacb02ed0225680669b4be16507cc4c138
sofiisunreal <sophiekendi17@gmail.com>
2026-07-03 13:07:18 +0300

    changes on rest framwork

--- motorhub_bck/settings.py
+++ motorhub_bck/settings.py
@@ -135,3 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK={
+    'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework_simplejwt.authentication.JWTAuthentication'),
+    'DEFAULT_PERMISSION_CLASSES':('rest_framework.permission.IsAuthenticated'),
+    'DEFAULT_SCHEMA_CLASS':('drf_spectacular.openapi.AutoSchema'),
+}
