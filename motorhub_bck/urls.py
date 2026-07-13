4ff75f8ac5650e2d5b5947b88ec4caa153bbc85f
Prince <princemurage@gmail.com>
2026-07-13 12:54:59 +0300

    schema

--- motorhub_bck/urls.py
+++ motorhub_bck/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -23,6 +24,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/',SpectacularSwaggerView.as_view(url_name='schema'),name='swagger-ui'),
 
     # auth
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
