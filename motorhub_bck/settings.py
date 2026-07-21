5720adb9494a98bf45b65f94d0fb0f4379dca515
sofiisunreal <sophiekendi17@gmail.com>
2026-07-21 19:01:53 +0300

    allowed hosts

--- motorhub_bck/settings.py
+++ motorhub_bck/settings.py
@@ -32,7 +32,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'mysql-sophiehiggs.alwaysdata.net',
+    'sophiehiggs.alwaysdata.net'
 
 ]
 
