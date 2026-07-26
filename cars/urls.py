02ebcda4a07a9edd9908f0c1ad1014b8f2d56437
sofiisunreal <sophiekendi17@gmail.com>
2026-07-26 11:01:26 +0300

    ca

--- cars/urls.py
+++ cars/urls.py
@@ -4,6 +4,6 @@ from .views import  AddCar, UpdateCar, ViewCars, UpdateCarStatus
 urlpatterns = [
     path('addcar/', AddCar),
     path('view_cars/', ViewCars),
-    path('updatecar/',UpdateCar),
+    path('updatecar/<int:id>/',UpdateCar),
     path('<int:id>/status/', UpdateCarStatus),
 ]
