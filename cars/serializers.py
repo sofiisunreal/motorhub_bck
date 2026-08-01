282450c612ccad47fd7e9ca6e4aed5de5343825a
sofiisunreal <sophiekendi17@gmail.com>
2026-08-01 19:44:43 +0300

    seriali

--- cars/serializers.py
+++ cars/serializers.py
@@ -1,9 +1,25 @@
-# serializers.py
-
 from rest_framework import serializers
+from suppliers.models import Supplier
 from .models import Car
 
 class CarSerializer(serializers.ModelSerializer):
+    supplier_id = serializers.PrimaryKeyRelatedField(
+        queryset=Supplier.objects.all(), source="supplier"
+    )
+    supplier_name = serializers.CharField(
+        source="supplier.company_name", read_only=True
+    )
+
     class Meta:
         model = Car
-        fields = "__all__"
+        fields = [
+            "id",
+            "supplier_id",
+            "supplier_name",
+            "brand",
+            "year",
+            "vin_number",
+            "buying_price",
+            "status",
+            "image",
+        ]
