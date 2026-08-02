9f985f4d31abcd709b6f288e5dbfc8541a351185
sofiisunreal <sophiekendi17@gmail.com>
2026-08-02 15:17:49 +0300

    supplier url

--- suppliers/urls.py
+++ suppliers/urls.py
@@ -1,10 +1,11 @@
 from django.db import router
 from django.urls import include, path
-from .views import  AddSupplier, UpdateSupplier, ViewSuppliers
+from .views import  AddSupplier, ToggleSupplierStatus, UpdateSupplier, ViewSuppliers
 from rest_framework.routers import DefaultRouter
 urlpatterns = [
     path('addsupplier/', AddSupplier),
     path('viewsuppliers/', ViewSuppliers),
     path('updatesupplier/<int:id>/', UpdateSupplier),
+    path("suppliers/<int:id>/toggle-status/",ToggleSupplierStatus),
 ]
 
