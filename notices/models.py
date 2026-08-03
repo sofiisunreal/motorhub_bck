7e0ecacb0c634199896e801aef64785eca2c373a
sofiisunreal <sophiekendi17@gmail.com>
2026-08-03 19:43:56 +0300

    notices

--- notices/models.py
+++ notices/models.py
@@ -5,9 +5,8 @@ from core.models import BaseModel
 
 class Notice(BaseModel):
     title = models.CharField(max_length=150)
-
     description = models.TextField()
-
+    is_archived=models.BooleanField(default=False)
     posted_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
