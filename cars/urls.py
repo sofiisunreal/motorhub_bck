6d745129831f4de4d2249edb2aa851d7fb1b7e3c
sofiisunreal <sophiekendi17@gmail.com>
2026-08-06 15:57:51 +0300

    oops

--- cars/urls.py
+++ cars/urls.py
@@ -3,7 +3,7 @@ from .views import  AddCar, UpdateCar, ViewCars, UpdateCarStatus,ViewSingleCar
 
 urlpatterns = [
     path('addcar/', AddCar),
-    path('view_cars/<int:id>/', ViewCars),
+    path('view_cars/', ViewCars),
     path('updatecar/<int:id>/',UpdateCar),
     path('<int:id>/status/', UpdateCarStatus),
     path('view_car/<int:id>/', ViewSingleCar)
